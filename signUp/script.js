81ddc0a9f24fac92eed0c6e96874f2eada45b203
duyguHsnHsn <duyguhsnhsn@gmail.com>
2021-12-31 12:12:13 +0200

    Script for adding new users
    I added the script for the Sign Up,  used
    Firebase realtime database to save the new user's username and password.

---
+++ signUp/script.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6i5_YsQ7r_LoHhAfzTBBqADOISO6bjgY",
+  authDomain: "chat-system-f5832.firebaseapp.com",
+  databaseURL:
+    "https://chat-system-f5832-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chat-system-f5832",
+  storageBucket: "chat-system-f5832.appspot.com",
+  messagingSenderId: "783006918118",
+  appId: "1:783006918118:web:cedd6c2443a74a05322227",
+  measurementId: "G-M45K3V7XVZ",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const userDataRef = ref(database, "users");
+const submit = document.getElementById("submitSignIn");
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  const newUserRef = push(userDataRef);
+  set(newUserRef, {
+    "username": username,
+    "password": password,
+  });
+  alert("Your account was created!");
+  document.getElementById("username").value = null;
+  document.getElementById("password").value = null;
+});
